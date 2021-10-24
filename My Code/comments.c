ed0a5afd5e6975c4e5f659fd78432aa63d91d97f
Paul Watts <7057417+PaulFWatts@users.noreply.github.com>
2021-10-25 07:38:29 +1100

    Added My Code folder and first programs

---
+++ My Code/comments.c
@@ -0,0 +1,11 @@
+int main(void)
+{
+    /* This is a comment in C */
+
+    /* This is a
+         multiline
+         comment in C */
+
+    // This is a C++ style comment
+    // This is a C++ style comment
+}
---
+++ My Code/template.c
@@ -0,0 +1,8 @@
+int main(void) {
+    /* This is a comment in C */
+
+    /* This is a
+         multiline
+         comment in C */
+
+}
