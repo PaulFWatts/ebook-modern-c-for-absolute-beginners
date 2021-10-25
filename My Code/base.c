17536b12c1ba7d7caf3b9d1a3ce602e894750c47
Paul Watts <7057417+PaulFWatts@users.noreply.github.com>
2021-10-25 12:08:16 +1100

    Added base.c

---
+++ My Code/base.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int x = 10; // decimal constant
+    int y = 012; // octal constant
+    int z = 0xA; // hexadecimal constant
+    printf("Decimal: %d  Octal: %o  Hexadecimal: %X\n", x,x,x);
+
+    return 0;
+}
+
+
